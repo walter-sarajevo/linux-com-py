4cea860a4b31f4d6e2d5a15c8d0f673f95e7f130
walter-sarajevo <walter_of_sarajevo@126.com>
2018-11-09 08:19:35 +0800

    step 1!

---
+++ CheckoutMethods.py
@@ -0,0 +1,28 @@
+# coding=utf-8
+
+class NoCheck(object):
+
+    def __init__(self):
+        self.description = "Use NO checkout method."
+        self.cbyte = ''
+        self.ccode = 0
+        self.method = "No, here's nothing."
+
+class Cover(object):
+
+    def __init__(self):
+        self.description = "Checkout method of upcover-module and downcover-module."
+        self.cbyte = ''
+        self.ccode = 0
+        self.method = "checkcode = [SUM( Value - 32) mod (95)] + 32"
+
+    def checkout(self, command):
+
+        for byte in command:
+            self.ccode += ord(byte) - 32
+
+        self.ccode = self.ccode % 95 + 32
+        self.cbyte = chr(self.ccode)
+
+        return self.cbyte
+
