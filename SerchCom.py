b882bcfab7981d2629cb247812edf24e65af4647
walter-sarajevo <walter_of_sarajevo@126.com>
2018-11-09 11:14:46 +0800

    com search

---
+++ SerchCom.py
@@ -0,0 +1,36 @@
+# coding=utf-8
+
+import serial
+import serial.tools.list_ports
+
+class SerchCom(object):
+
+    # 
+    
+    def __init__(self):
+        self.description = "Method to seach com in this machine."
+        self.port_list = []
+        self.port_count = 0
+        self.error = 0
+
+        ports = serial.tools.list_ports.comports()
+        for i in range(0, len(ports)):
+            name = str(ports[i]).split(' ', 1)
+            self.port_list.append(name[i])
+        self.port_count = len(ports)
+
+    # def __call__(self):
+    #     self.description = "Method to seach com in this machine."
+    #     self.port_list = []
+    #     self.port_count = 0
+    #     self.error = 0
+
+    #     # ports = list(serial.tools.list_ports.comports())
+    #     ports = serial.tools.list_ports.comports()
+    #     for i in range(0, len(ports)):
+    #         name = str(ports[i]).split(' ', 1)
+    #         self.port_list.append(name[i])
+    #     # self.port_list = name.split(' ', 1)
+    #     self.port_count = len(ports)
+    #     print (self.port_list)
+
---
+++ testsearchcom.py
@@ -0,0 +1,9 @@
+import serial
+import serial.tools.list_ports
+import SerchCom
+
+# a = SerchCom.SerchCom()
+# b = serial.tools.list_ports.comports()
+# print(a.port_list)              
+
+SerchCom.SerchCom()
